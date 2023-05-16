8c44cd10f5204d0c83a2d9ae881a1dc74ef63ab5
Newceo <petalpurpose@gmail.com>
2023-05-16 08:37:56 +0100

    square one again

--- src/Pages/SignIn.js
+++ src/Pages/SignIn.js
@@ -41,25 +41,6 @@ const SignIn = () => {
 
         // Store form data in URL parameters
         navigate(`/dashboard?name=${formData.name}&email=${formData.email}`);
-
-        const sgMail = require('@sendgrid/mail')
-        const API_KEY = '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';
-        sgMail.setApiKey(API_KEY)
-        const msg = {
-            to: `${formData.email}`, // Change to your recipient
-            from: 'dskafos@dskafos.com', // Change to your verified sender
-            subject: 'Sending with SendGrid is Fun',
-            text: 'and easy to do anywhere, even with Node.js',
-            html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-        }
-        sgMail
-            .send(msg)
-            .then(() => {
-                console.log('Email sent')
-            })
-            .catch((error) => {
-                console.error(error)
-            })
     };
 
     const sgMail = require('@sendgrid/mail')
